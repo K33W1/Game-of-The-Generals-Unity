cb3c01863c6fc9a14cab1380f319fe126b04d3c0
K33W1 <bruceakimiranda@gmail.com>
2020-10-08 15:26:30 +0800

    Fixed wrong game output

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -193,8 +193,8 @@ public class Board : MonoBehaviour
 
     private void EndGame(GameOutput gameOutput)
     {
-        CurrentGamePhase.Value = GamePhase.End;
         CurrentGameOutput = gameOutput;
+        CurrentGamePhase.Value = GamePhase.End;
     }
 
     private bool TryMovePiece(MoveInfo move)
