d30ea0c17af3c2e318884e763ae8d1a867574195
K33W1 <bruceakimiranda@gmail.com>
2020-10-11 16:36:02 +0800

    Added "Flip" extension method for Side enum

---
+++ Assets/Scripts/Board/Enums/SideExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+public static class SideExtensions
+{
+    public static Side Flip(this Side side)
+    {
+        Debug.Assert(side != Side.None);
+        return side == Side.A ? Side.B : Side.A;
+    }
+}
